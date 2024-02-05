44b46f40ed51fee3d253d58b5b4fea4887e706a2
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2024-02-05 23:24:53 +0200

    Start 7day

--- src/components/MySuccess/MySuccess.jsx
+++ src/components/MySuccess/MySuccess.jsx
@@ -20,7 +20,7 @@ const CustomizedRating = ({ onChangeRating }) => {
       name="customized-color"
       value={value}
       onChange={handleChange}
-      getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+      getLabelText={(value) => `${value * 2} бали`}
       precision={0.5}
       max={3}
       icon={<FavoriteIcon fontSize="inherit" />}
@@ -33,11 +33,12 @@ const HabitTracker = () => {
   const [progress, setProgress] = useState(0);
   const [activeDay, setActiveDay] = useState(0);
   const [buttonActivity, setButtonActivity] = useState(Array(7).fill(false));
+  const [totalRating, setTotalRating] = useState(0);
 
   const handleDayClick = (day) => {
-    if (day === activeDay) return;
+    if (day === activeDay || buttonActivity[day - 1]) return;
 
-    setProgress((prevProgress) => prevProgress + 1);
+    setProgress((prevProgress) => prevProgress + (1 / 21) * 100);
     setActiveDay(day);
     setButtonActivity((prev) => {
       const newButtonActivity = [...prev];
@@ -46,7 +47,6 @@ const HabitTracker = () => {
       if (newButtonActivity.every((button) => button)) {
         setProgress(0);
         setActiveDay(0);
-        alert('Вітаю, ти сформував нову корисну звичку!');
       }
 
       return newButtonActivity;
@@ -54,12 +54,8 @@ const HabitTracker = () => {
   };
 
   const handleRatingChange = (value) => {
-    // Якщо рейтинг досягає 3, але не активовані всі дні, то теж скидаємо
-    if (value === 3 && !buttonActivity.includes(false)) {
-      setProgress(0);
-      setActiveDay(0);
-      alert('Вітаю, ти сформував нову корисну звичку!');
-    }
+    // Додаємо 2 бали за кожне заповнене сердечко
+    setTotalRating((prev) => prev + value * 2);
   };
 
   const activeButtonCount = buttonActivity.filter(Boolean).length;
@@ -69,10 +65,13 @@ const HabitTracker = () => {
       <Typography variant="h6" sx={{ mb: 2 }}>
         Дні активованих кнопок: {activeButtonCount}
       </Typography>
+      <Typography variant="h6" sx={{ mb: 2 }}>
+        Бали: {totalRating}
+      </Typography>
       <LinearProgress
         variant="determinate"
-        value={(progress / 21) * 100}
-        sx={{ height: '20px', mb: 2 }}
+        value={progress}
+        sx={{ width: '100%', height: '20px', mb: 2 }}
       />
       {[1, 2, 3, 4, 5, 6, 7].map((day, index) => (
         <Box key={day} sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
