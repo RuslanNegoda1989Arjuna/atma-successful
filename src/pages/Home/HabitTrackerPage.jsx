6b5dcf6881106d0576213f9c5e59f32e01cc6720
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2024-03-15 13:29:02 +0200

    add input

--- src/pages/Home/HabitTrackerPage.jsx
+++ src/pages/Home/HabitTrackerPage.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from 'hooks/use-auth';
 import { Navigate } from 'react-router-dom';
 import HabitTracker from 'components/HabitTracker/HabitTracker';
 import { WelcomeTitle } from './HabitTrackerPage.styled';
 
-// Star icon
+const HabitInput = ({ onCreate }) => {
+  const [habitName, setHabitName] = useState('');
+
+  const handleInputChange = (event) => {
+    setHabitName(event.target.value);
+  };
+
+  const handleCreateClick = () => {
+    onCreate(habitName);
+    setHabitName('');
+  };
+
+  return (
+    <div>
+      <input type="text" value={habitName} onChange={handleInputChange} />
+      <button onClick={handleCreateClick}>Create</button>
+    </div>
+  );
+};
 
 
 const HabitTrackerPage = () => {
   const { isAuth, email, displayName } = useAuth();
+  const [newHabit, setNewHabit] = useState('');
+
+const handleHabitCreate = (habitName) => {
+    setNewHabit(habitName);
+  };
 
   return isAuth ? (
     <div>
       <WelcomeTitle>Welcome {displayName || email}</WelcomeTitle>
+      {newHabit ? (
+        <h2>{newHabit}</h2>
+      ) : (
+        <HabitInput onCreate={handleHabitCreate} />
+      )}
       <HabitTracker/>
     </div>
   ) : (
